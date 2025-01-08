81fca5c3f8f860df18a5c263ecaa746999269dcd
Juan D Hernandez G <juandavos941@gmail.com>
2025-01-07 20:01:32 -0500

    Initial setup

---
+++ layers/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="llamaindex_custom_utils",
+    version="0.0.1",
+    description="LlamaIndex Custom Utils",
+    url="",
+    author="DavosCode",
+    author_email="jdhernandez@azzertai.com",
+    license="MIT",
+    packages=["llamaindex_custom_utils",],
+    zip_safe=False,
+)
